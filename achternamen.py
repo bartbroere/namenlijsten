571efaedad7d3e5c3f944d9c898025538f30fa0b
Bart Broere <mail@bartbroere.eu>
2019-12-29 18:58:43 +0100

    Parse JavaScript

--- achternamen.py
+++ achternamen.py
@@ -1,16 +1,32 @@
 import string
-from collections import namedtuple
+from collections import namedtuple, deque
 from random import shuffle
 
 import bs4
 import pandas
 import requests
 import requests_cache
+from slimit import ast
+from slimit.parser import Parser
+from slimit.visitors import nodevisitor
 from tqdm import tqdm
 
 
 def get_directory(offset, letter, treffers):
     return requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?offset={offset}&naam={letter}&treffers={treffers}&operator=bw')
+
+
+def parse_javascript(data):
+    parser = Parser()
+    tree = parser.parse(data)
+    strings_in_javascript = deque(x.value for x in nodevisitor.visit(tree) if isinstance(x, ast.String))
+    councils = {}
+    if "'GoogleAnalyticsObject'" not in strings_in_javascript:
+        while strings_in_javascript:
+            key = strings_in_javascript.popleft()
+            value = int(strings_in_javascript.popleft().replace(f"{key[:-1]} (", '').replace(")'", ""))
+            councils[key[1:-1]] = value
+    return councils
 
 
 def add_gemeenten(row):
@@ -23,17 +39,20 @@ def add_gemeenten(row):
         except IndexError:
             pass
         try:
-            rel_detail_page = detail_page.select('td.justification-left ~ td.justification-left > b > a')[0].get('href')
+            rel_detail_page = detail_page.select('td.justification-left ~ td.justification-left > a')[0].get('href')
             rel_detail_page = requests.get(f'https://www.cbgfamilienamen.nl/nfb/{rel_detail_page}').text
             rel_detail_page = bs4.BeautifulSoup(rel_detail_page, features='html.parser')
             rel_map_image = rel_detail_page.select('img[usemap]')[0].get('src')
             requests.get(f'https://www.cbgfamilienamen.nl/{rel_map_image}')
         except:
             pass
+        # TODO turned off, this needs to be done in parallel
         # try:
-        #     detail_page.select('script:not([async])')[0].text
+        #     row['gemeenten'] = parse_javascript(detail_page.select('script:not([async])')[0].text)
+        #     print(row['gemeenten'])
         # except IndexError:
         #     pass
+    return row
 
 
 if __name__ == '__main__':
@@ -67,5 +86,6 @@ if __name__ == '__main__':
                 achternamen.append(AchternaamRecord(achternaam, counts, link))
 
     achternamen = pandas.DataFrame(achternamen)
-    achternamen.to_csv('achternamen.csv.gz', compression='gzip')
     achternamen.progress_apply(add_gemeenten, axis=1)
+    achternamen.to_csv('achternamen.csv.gz', compression='gzip')
+    achternamen.to_csv('achternamen.csv')
