0f6453c2f87b14784133c1d3ac4eb50d57fad185
Bart Broere <mail@bartbroere.eu>
2019-12-29 12:18:46 +0100

    Download directory and detail pages

---
+++ achternamen.py
@@ -0,0 +1,64 @@
+import hashlib
+import string
+from collections import namedtuple
+from glob import glob
+from random import shuffle
+
+import bs4
+import pandas
+import requests
+import requests_cache
+from ratelimit import limits, sleep_and_retry
+from tqdm import tqdm
+
+requests_cache.install_cache('meertens')
+tqdm.pandas()
+
+letters = list(string.ascii_lowercase)
+shuffle(letters)
+
+
+@sleep_and_retry
+@limits(calls=1, period=4)
+def get_directory(offset, letter, treffers):
+    return requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?offset={offset}&naam={letter}&treffers={treffers}&operator=bw')
+
+
+def download_page(link):
+    hashed_link = hashlib.md5()
+    hashed_link.update(link.encode('utf8'))
+    with open('data/' + hashed_link.hexdigest(), 'w') as w:
+        w.write(requests.get(f'https://www.cbgfamilienamen.nl/nfb/{link}').text)
+
+
+for letter in tqdm(letters):
+    offset = 0
+    treffers = int(requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?operator=bw&naam={letter}').text.split("treffers=")[1].split("&")[0])
+    while offset + 50 < treffers:
+        offset += 50
+        data = get_directory(offset, letter, treffers)
+        with open(f'data/{letter}-{offset}.html', 'w') as w:
+            w.write(data.text)
+
+
+AchternaamRecord = namedtuple("AchernaamRecord", ('achternaam', 'counts', 'link'))
+achternamen = []
+
+for filename in glob('data/*-*.html'):
+    data = bs4.BeautifulSoup(open(filename, 'r').read())
+
+    for achternaam in data.select('td.justification-right'):
+        tr = achternaam.parent
+        fields = tr.select('td')
+        achternaam = fields[0].text.strip()
+        try:
+            link = fields[0].select('a')[0].get('href')
+        except:
+            link = None
+        counts = fields[1].text.strip()
+        achternamen.append(AchternaamRecord(achternaam, counts, link))
+
+achternamen = pandas.DataFrame(achternamen)
+achternamen.to_csv('achternamen.csv')
+
+achternamen.link.dropna().progress_apply(download_page)
