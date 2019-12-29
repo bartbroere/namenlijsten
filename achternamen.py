e4da23530c54f2f3ea37132d98777ff94a59fb31
Bart Broere <mail@bartbroere.eu>
2019-12-29 12:50:16 +0100

    Begin parsing detail page

--- achternamen.py
+++ achternamen.py
@@ -11,12 +11,6 @@ import requests_cache
 from ratelimit import limits, sleep_and_retry
 from tqdm import tqdm
 
-requests_cache.install_cache('meertens')
-tqdm.pandas()
-
-letters = list(string.ascii_lowercase)
-shuffle(letters)
-
 
 @sleep_and_retry
 @limits(calls=1, period=4)
@@ -31,34 +25,52 @@ def download_page(link):
         w.write(requests.get(f'https://www.cbgfamilienamen.nl/nfb/{link}').text)
 
 
-for letter in tqdm(letters):
-    offset = 0
-    treffers = int(requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?operator=bw&naam={letter}').text.split("treffers=")[1].split("&")[0])
-    while offset + 50 < treffers:
-        offset += 50
-        data = get_directory(offset, letter, treffers)
-        with open(f'data/{letter}-{offset}.html', 'w') as w:
-            w.write(data.text)
+def add_gemeenten(row):
+    if row.link is not None:
+        hashed_link = hashlib.md5()
+        hashed_link.update(row.link.encode('utf8'))
+        with open('data/' + hashed_link.hexdigest(), 'r') as f:
+            detail_page = bs4.BeautifulSoup(f.read())
+        print(detail_page.find('script:not([async])')[0].text)
 
 
-AchternaamRecord = namedtuple("AchernaamRecord", ('achternaam', 'counts', 'link'))
-achternamen = []
+if __name__ == '__main__':
+    requests_cache.install_cache('meertens')
+    tqdm.pandas()
 
-for filename in glob('data/*-*.html'):
-    data = bs4.BeautifulSoup(open(filename, 'r').read())
+    letters = list(string.ascii_lowercase)
+    shuffle(letters)
 
-    for achternaam in data.select('td.justification-right'):
-        tr = achternaam.parent
-        fields = tr.select('td')
-        achternaam = fields[0].text.strip()
-        try:
-            link = fields[0].select('a')[0].get('href')
-        except:
-            link = None
-        counts = fields[1].text.strip()
-        achternamen.append(AchternaamRecord(achternaam, counts, link))
+    for letter in tqdm(letters):
+        offset = 0
+        treffers = int(
+            requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?operator=bw&naam={letter}').text.split(
+                "treffers=")[1].split("&")[0])
+        while offset + 50 < treffers:
+            offset += 50
+            data = get_directory(offset, letter, treffers)
+            with open(f'data/{letter}-{offset}.html', 'w') as w:
+                w.write(data.text)
 
-achternamen = pandas.DataFrame(achternamen)
-achternamen.to_csv('achternamen.csv')
+    AchternaamRecord = namedtuple("AchernaamRecord", ('achternaam', 'counts', 'link'))
+    achternamen = []
 
-achternamen.link.dropna().progress_apply(download_page)
+    for filename in glob('data/*-*.html'):
+        data = bs4.BeautifulSoup(open(filename, 'r').read())
+
+        for achternaam in data.select('td.justification-right'):
+            tr = achternaam.parent
+            fields = tr.select('td')
+            achternaam = fields[0].text.strip()
+            try:
+                link = fields[0].select('a')[0].get('href')
+            except:
+                link = None
+            counts = fields[1].text.strip()
+            achternamen.append(AchternaamRecord(achternaam, counts, link))
+
+    achternamen = pandas.DataFrame(achternamen)
+    achternamen.to_csv('achternamen.csv')
+
+    achternamen.link.dropna().progress_apply(download_page)
+    achternamen.link.dropna().progress_apply(add_gemeenten)
