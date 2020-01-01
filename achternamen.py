b44013010699417c1aacd8ed0c6aa47b5995936b
Bart Broere <mail@bartbroere.eu>
2020-01-01 15:51:59 +0100

    Count pixel values

--- achternamen.py
+++ achternamen.py
@@ -1,16 +1,25 @@
+import io
+import os
 import string
-from collections import namedtuple, deque
+from collections import deque, Counter, namedtuple
 from random import shuffle
+from tempfile import TemporaryDirectory
 
 import bs4
+import numpy
 import pandas
+import rasterio
 import requests
 import requests_cache
+from PIL import Image
+from more_itertools import grouper
+from pandarallel import pandarallel
+from rasterio.mask import raster_geometry_mask
+from shapely.geometry import Polygon
 from slimit import ast
 from slimit.parser import Parser
 from slimit.visitors import nodevisitor
 from tqdm import tqdm
-from pandarallel import pandarallel
 
 
 def get_directory(offset, letter, treffers):
@@ -31,25 +40,41 @@ def parse_javascript(javascript_code):
 
 
 def add_gemeenten(row):
+    directory = TemporaryDirectory()
     if row.link is not None:
         detail_page = requests.get(f'https://www.cbgfamilienamen.nl/nfb/{row.link}').text
         detail_page = bs4.BeautifulSoup(detail_page, features="html.parser")
         try:
-            map_image = detail_page.select('img[usemap]')[0].get('src')
-            requests.get(f'https://www.cbgfamilienamen.nl/{map_image}')
+            row['gemeenten'] = parse_javascript(detail_page.select('script:not([async])')[0].text)
         except IndexError:
             pass
         try:
             rel_detail_page = detail_page.select('td.justification-left ~ td.justification-left > a')[0].get('href')
             rel_detail_page = requests.get(f'https://www.cbgfamilienamen.nl/nfb/{rel_detail_page}').text
             rel_detail_page = bs4.BeautifulSoup(rel_detail_page, features='html.parser')
-            rel_map_image = rel_detail_page.select('img[usemap]')[0].get('src')
-            requests.get(f'https://www.cbgfamilienamen.nl/{rel_map_image}')
-        except:
-            pass
-        # TODO turned off, this needs to be done in parallel
+            detail_pages = [detail_page, rel_detail_page]
+        except IndexError:
+            detail_pages = [detail_page]
         try:
-            row['gemeenten'] = parse_javascript(detail_page.select('script:not([async])')[0].text)
+            for detail_page in detail_pages:
+                map_image = detail_page.select('img[usemap]')[0].get('src')
+                url = f'https://www.cbgfamilienamen.nl/{map_image}'
+                image_data = requests.get(url).content
+                with open(os.path.join(directory.name, url.split("=")[-1]), 'wb') as w:
+                    w.write(image_data)
+                image = Image.open(io.BytesIO(image_data))
+                raster = rasterio.open(os.path.join(directory.name, url.split("=")[-1]))
+                gemeenten = {}
+                for area in detail_page.select('map > area'):
+                    points = deque(int(x) for x in area.get('coords').split(','))
+                    gemeente_mask = raster_geometry_mask(raster, [Polygon(grouper(points, 2))], invert=True)
+                    pixel_counter = Counter(numpy.array(image)[gemeente_mask[0]])
+                    gemeenten[area.get('alt')] = pixel_counter
+                os.unlink(os.path.join(directory.name, url.split("=")[-1]))
+                if 'abs.png' in url:
+                    row['abs_pixel_counters'] = gemeenten
+                if 'rel.png' in url:
+                    row['rel_pixel_counters'] = gemeenten
         except IndexError:
             pass
     return row
@@ -65,9 +90,8 @@ if __name__ == '__main__':
 
     AchternaamRecord = namedtuple("AchernaamRecord", ('achternaam', 'counts', 'link'))
     achternamen = []
-    # letters = ['be']
 
-    for letter in tqdm(letters):
+    for letter in tqdm(['broe']):
         offset = 0
         treffers = int(
             requests.get(f'https://www.cbgfamilienamen.nl/nfb/lijst_namen.php?operator=bw&naam={letter}').text.split(
@@ -82,12 +106,12 @@ if __name__ == '__main__':
                 achternaam = fields[0].text.strip()
                 try:
                     link = fields[0].select('a')[0].get('href')
-                except:
+                except IndexError:
                     link = None
                 counts = fields[1].text.strip()
                 achternamen.append(AchternaamRecord(achternaam, counts, link))
 
     achternamen = pandas.DataFrame(achternamen)
-    achternamen.parallel_apply(add_gemeenten, axis=1)
+    achternamen = achternamen.progress_apply(add_gemeenten, axis=1)
     achternamen.to_csv('achternamen.csv.gz', compression='gzip')
     achternamen.to_csv('achternamen.csv')
